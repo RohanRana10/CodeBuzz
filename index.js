4e09f23659d8a4a2626a56c2f34106d9cb20f329
Rohan Rana <rohanrana.mail@gmail.com>
2022-10-15 17:38:44 +0530

    set up ejs view engine

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 8000;
 app.use('/',require('./routes/index'));
 app.use('/users',require('./routes/users'));
 
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port,function(err){
     if(err){
         console.log(`Error in listening to the port: ${err}`);
