5a4b055d41897fed693c4d0504983a8107b152dc
Rohan Rana <rohanrana.mail@gmail.com>
2022-10-16 17:17:05 +0530

    link mongoDB

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ app.use(express.static('./assets'));
 
 const expressLayouts = require('express-ejs-layouts');
 app.use(expressLayouts);
+
+const db = require('./config/mongoose');
+
 //extract styles and scripts from sub pages into the layouts
 app.set('layout extractStyles',true);
 app.set('layout extractScripts',true);
@@ -13,7 +16,9 @@ app.set('layout extractScripts',true);
 app.use('/',require('./routes/index'));
 app.use('/users',require('./routes/users'));
 
+//set view engine as ejs
 app.set('view engine','ejs');
+//where to look for views
 app.set('views','./views');
 
 app.listen(port,function(err){
