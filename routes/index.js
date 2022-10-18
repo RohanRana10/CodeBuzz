8938b9dda40d481dda029d630f315e825533a5ba
Rohan Rana <rohanrana.mail@gmail.com>
2022-10-19 01:16:58 +0530

    add comments functionality

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const homeController = require('../controllers/home_controller');
 router.get('/',homeController.home);
 
 router.use('/posts', require('./posts'));
+router.use('/comments',require('./comments'));
 
 
 console.log(`Router loaded`);
